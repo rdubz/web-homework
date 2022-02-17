0c6adb65baf98e8d1bbcf7f8dfbff19925b06885
rdubz <rdubz.801smashin@gmail.com>
2022-02-17 15:02:44 -0700

    Fixed bug where debit/credit data wasn't displaying on TXChart. Also restyled this chart with m-ui chart

--- webapp/src/components/transactions/TxTable.js
+++ webapp/src/components/transactions/TxTable.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { arrayOf, string, bool, number, shape } from 'prop-types'
 import { css } from '@emotion/core'
+import Converter from '../../utils/converter'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { transactions } from '../../../mocks/transactions-data';
 
 const styles = css`
- .transactions {
-   margin: 0 auto;
- }
  .header {
    font-weight: bold;
  }
@@ -14,37 +20,50 @@ const styles = css`
 const makeDataTestId = (transactionId, fieldName) => `transaction-${transactionId}-${fieldName}`
 
 export function TxTable ({ data }) {
-  return (
-    <table className='transactions' css={styles}>
-      <tbody>
-        <tr className='header'>
-          <td >ID</td>
-          <td >User ID</td>
-          <td >Description</td>
-          <td >Merchant ID</td>
-          <td >Debit</td>
-          <td >Credit</td>
-          <td >Amount</td>
-        </tr>
-        {
-          data.map(tx => {
-            const { id, user_id: userId, description, merchant_id: merchantId, debit, credit, amount } = tx
-            return (
-              <tr data-testid={`transaction-${id}`} key={`transaction-${id}`}>
-                <td data-testid={makeDataTestId(id, 'id')}>{id}</td>
-                <td data-testid={makeDataTestId(id, 'userId')}>{userId}</td>
-                <td data-testid={makeDataTestId(id, 'description')}>{description}</td>
-                <td data-testid={makeDataTestId(id, 'merchant')}>{merchantId}</td>
-                <td data-testid={makeDataTestId(id, 'debit')}>{debit}</td>
-                <td data-testid={makeDataTestId(id, 'credit')}>{credit}</td>
-                <td data-testid={makeDataTestId(id, 'amount')}>{amount}</td>
-              </tr>
-            )
-          })
-        }
-      </tbody>
-    </table>
 
+  function RomanNumerals()
+  {
+    let enabled = localStorage.getItem('displayRomanNumerals')
+
+        if(enabled === null || enabled === 'false') {
+            return false;
+        }
+        
+        return true;
+  }
+
+  return (
+          <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell align="right">User ID</TableCell>
+                <TableCell align="right">Description</TableCell>
+                <TableCell align="right">Merchant ID</TableCell>
+                <TableCell align="right">Debit</TableCell>
+                <TableCell align="right">Credit</TableCell>
+                <TableCell align="right">Amount</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map((tx) => (
+                <TableRow
+                  key={tx.id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell data-testid={makeDataTestId(tx.id, 'id')} component="th" scope="row"> {tx.id}</TableCell>
+                  <TableCell data-testid={makeDataTestId(tx.id, 'userId')} align="right">{tx.user_id}</TableCell>
+                  <TableCell data-testid={makeDataTestId(tx.id, 'description')} align="right">{tx.description}</TableCell>
+                  <TableCell data-testid={makeDataTestId(tx.id, 'merchant')} align="right">{tx.merchant_id}</TableCell>
+                  <TableCell data-testid={makeDataTestId(tx.id, 'debit')} align="right">{tx.debit? 'Yes' : 'No'}</TableCell>
+                  <TableCell data-testid={makeDataTestId(tx.id, 'credit')} align="right">{tx.credit? 'Yes' : 'No'}</TableCell>
+                  <TableCell data-testid={makeDataTestId(tx.id, 'amount')} align="right">{RomanNumerals()? Converter.ToRomanNumerals(tx.amount) : tx.amount}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
   )
 }
 
--- webapp/src/home/home-page.js
+++ webapp/src/home/home-page.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import { useQuery } from '@apollo/client'
 import GetTransactions from '../gql/transactions.gql'
 import { Transactions } from '../components/transactions/Transactions'
+import { transactions } from '../../mocks/transactions-data'
 
 export function Home () {
   const { loading, error, data = {} } = useQuery(GetTransactions)
